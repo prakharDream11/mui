ca44b072e1ed15c1a04f7b4d60e8d6378523f181
Prakhar Dixit <prakhar.dixit@dream11.com>
2022-01-16 20:58:38 +0530

    testing page load time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Alert from '@mui/material/Alert';
+import Pagination from '@mui/material/Pagination';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Button variant="text">Text</Button>
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    <Alert severity="error">This is an error alert — check it out!</Alert>
+<Pagination count={10} />
+<CircularProgress />
+
+    </>
   );
 }
 
